8a41400266b0398ce5a49548a7cc59b3146f6f3f
dmncp <damiancyper@gmail.com>
2022-02-06 20:42:18 +0100

    rwd implementation; scroll 'contact' and 'about me' section animations

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,49 +1,55 @@
+let aboutMe = document.getElementsByClassName('desc')[0];
+let contactOptions = document.getElementById('contact-options');
+let offset = 800;
+let cards = document.getElementsByClassName("project-item");
+
+
 document.addEventListener("scroll", function (){
     let h = window.screen.availHeight;
 
     titles.forEach((title, id) =>{
-        let titleOffset = title.getBoundingClientRect().top;
+        setAnimation(title, setPosition, "0", "-100%");
 
-        if(titleOffset <= 400){
-            title.style.left = `0`;
-        }
-        else{
-            title.style.left = '-100%';
-        }
-        if(titleOffset <= 400 && titleOffset >= 0){ // if time to change page nr
+        if(title.getBoundingClientRect().top <= offset && title.getBoundingClientRect().top >= 0){ // if time to change page nr
             // change page number
             pageNr.firstChild.textContent = `0${id+2}`
         }
-
     })
 
     // if user go back to home page
     if(titles[0].getBoundingClientRect().top >= h){
         pageNr.firstChild.textContent = '01';
     }
-    if(titles[1].getBoundingClientRect().top <= 400){
-        gridItems.forEach(item => item.style.transform = "scale(1)");
-    }
-    else{
-        gridItems.forEach(item => item.style.transform = "scale(0)");
-    }
+
+    gridItems.forEach(item =>{
+        setAnimation(item, setScale, "scale(1)", "scale(0)");
+    })
+
+    setAnimation(aboutMe, setScale, "scale(1)", "scale(0)");
+    setAnimation(contactOptions, setScale, "scale(1)", "scale(0)");
+
 
     //projects card animation
-    let cards = document.getElementsByClassName("project-item");
-    let firstCardOffset = cards[0].getBoundingClientRect().top;
-    let secondCardOffset = cards[1].getBoundingClientRect().top;
-
-    if(firstCardOffset <= 400){
-        cards[0].style.left = '0';
-    }
-    else{
-        cards[0].style.left = '-100%';
-    }
-
-    if(secondCardOffset <= 400){
-        cards[1].style.left = '0';
-    }
-    else{
-        cards[1].style.left = '100%';
-    }
+    setAnimation(cards[0], setPosition, "0", "-100%");
+    setAnimation(cards[1], setPosition, "0", "100%");
 })
+
+
+function setScale(element, val){
+    element.style.transform = val;
+}
+
+function setPosition(element, val){
+    element.style.left = val;
+}
+
+function setAnimation(element, fun, val1, val2){
+    let elemOffset = element.getBoundingClientRect().top;
+
+    if(elemOffset <= offset){
+        fun(element, val1);
+    }
+    else{
+        fun(element, val2);
+    }
+}
